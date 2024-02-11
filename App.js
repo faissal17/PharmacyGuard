3234589b7b622c10ea634d70ad77c6b41e86db91
faissal17 <faissalaoukacha@gmail.com>
2024-02-11 15:36:32 +0100

    start editing login page

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import LoginScreen from './src/components/screens/LoginScreen';
+import Logo from './assets/images/pharmacy.png';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your wtf!</Text>
+    <View>
+      <Image source={Logo} style={logoStyle.container} resizeMode="contain" />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const logoStyle = StyleSheet.create({
+  container: {
+    width: '70%',
+    height: 100,
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
