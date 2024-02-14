159d665da7c43fc0f3be8335bb2c1fc43014dc00
faissal17 <faissalaoukacha@gmail.com>
2024-02-14 15:10:50 +0100

    add all the necessary screens

---
+++ src/components/screens/PharmacyDetailsScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PharmacyDetailsScreen = () => {
+  return (
+    <View>
+      <Text>PharmacyDetailsScreen</Text>
+    </View>
+  )
+}
+
+export default PharmacyDetailsScreen
---
+++ src/components/screens/PharmacyListScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PharmacyListScreen = () => {
+  return (
+    <View>
+      <Text>PharmacyListScreen</Text>
+    </View>
+  )
+}
+
+export default PharmacyListScreen
